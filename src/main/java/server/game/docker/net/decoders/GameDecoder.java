41953c9b86fb4ce736dc277096fdab77acc630da
Radovan Monček <radovan.moncek1@gmail.com>
2024-10-08 23:03:31 +0200

    fix(decoders): partially implement fix for correct decoding with the protocol identifier included in the header

--- src/main/java/server/game/docker/net/decoders/GameDecoder.java
+++ src/main/java/server/game/docker/net/decoders/GameDecoder.java
@@ -14,26 +14,30 @@ public class GameDecoder extends ByteToMessageDecoder {
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) {
         //todo: Check protocol identifier, else drop
-        if(in.readableBytes() >= 3) {
+        if(in.readableBytes() >= 3){
             in.markReaderIndex();
             if(
                     (int) in.readUnsignedByte() != 'F'
                             || (int) in.readUnsignedByte() != 'E'
                             || (int) in.readUnsignedByte() != 'D'
-            )
+            ) {
                 return;
-        }
-        //Await first "magic" byte for PDUType identification
-        if(in.readableBytes() >= 4){
-            in.markReaderIndex();
+            }
+
+            //Await "magic" byte for PDUType identification
+            if(in.readableBytes() < 4) {
+                return;
+            }
+
             PDUType type = PDUType.valueOf((byte) in.readUnsignedByte());
 
-            //If invalid tagged PDU, drop
-            if(type.equals(PDUType.INVALID))
+            //If PDU tagged invalid, drop
+            if(type.equals(PDUType.INVALID)) {
                 return;
+            }
 
-            //Determine, if any PDU body data are to be transported (their length is based on Integer length identifier which has to be awaited)
-            if(!type.isEmpty() && (in.readableBytes() >= 5) && (in.readableBytes() < in.readInt())) {
+            //Determine, if any PDU body data are to be transported (their length is based on an Integer length identifier (4B) which has to be awaited)
+            if(!type.isEmpty() && (in.readableBytes() >= 8) && (in.readableBytes() < in.readInt())) {
                 in.resetReaderIndex();
                 return;
             }
